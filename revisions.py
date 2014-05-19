e89580685f951a088ab773b2f285a2a281310006
Baltazar-U <laurent.dardelet@ens.fr>
2014-05-19 00:20:50 -0700

    Started the revisions files

---
+++ revisions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+class Revision:
+    def __init__(self, number=0):
+        name='revisions\\rev_'+str(number)+'.chg'
+        save=False
+        self.musicdata={}
+        files=[]
+        self.doublets=False
+        
+        try:
+            revisionfile=open(name,'r')
+        except:
+            revisionfile=open(name,'w')
+            save=True
+        if not save:
+            for change in revisionfile:
+                entries=change.split('&')
+                if entries[1] not in self.musicdata:
+                    self.musicdata[entries[1]]={}
+                if entries[2] not in self.musicdata[entries[1]]:
+                    self.musicdata[entries[1]][entries[2]]=[]
+                if entries[3] in self.musicdata[entries[1]][entries[2]]:
+                    self.doublets=True
+                else:
+                    self.musicdata[entries[1]][entries[2]]+=[[entries[3], entries[0]]]
+        
